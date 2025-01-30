1cb3e7e95b0366eb92de19541df5f3bc93a90447
Student <Student@Train11-3-PC200.control.ad.sit.kmutt.ac.th>
2025-01-30 14:18:16 +0700

    add code DTO_01

---
+++ DTO_thu/src/main/java/sit/int202/dto_thu/DtoThuApplication.java
@@ -0,0 +1,13 @@
+package sit.int202.dto_thu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoThuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtoThuApplication.class, args);
+    }
+
+}
