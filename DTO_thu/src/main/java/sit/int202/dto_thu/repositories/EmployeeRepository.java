1da1710e5d5ff92c115634fbf634a12228147a26
Chanidapaka <chanidapapanta@gmail.com>
2025-03-28 22:47:42 +0700

    edit interface

--- DTO_thu/src/main/java/sit/int202/dto_thu/repositories/EmployeeRepository.java
+++ DTO_thu/src/main/java/sit/int202/dto_thu/repositories/EmployeeRepository.java
@@ -1,7 +1,7 @@
 package sit.int202.dto_thu.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import sit.int202.dto_thu.entities.Employee;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
--- DTO_thu/src/main/java/sit/int202/dto_thu/repositories/OrderRepository.java
+++ DTO_thu/src/main/java/sit/int202/dto_thu/repositories/OrderRepository.java
@@ -1,7 +1,7 @@
 package sit.int202.dto_thu.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import sit.int202.dto_thu.entities.Order;
 
-public interface OrderRepository extends CrudRepository<Order, Integer> {
+public interface OrderRepository extends JpaRepository<Order, Integer> {
 }
